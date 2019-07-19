1a95fe952db5d6022b84f600f1ce040d8fae1cd0
Isa Omarzada <isaomarzada@yahoo.com>
2019-07-19 13:12:44 -0500

    no css

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app     = express()
+const path    = require('path')
+const port    = 3000
+
+
+app.use('/css', express.static(__dirname +'/css'));
+app.use(express.static('grocery-app'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/grocery.html')))
+
+app.listen(process.env.PORT || port, () => console.log(`App listening on port ${port}!`));
